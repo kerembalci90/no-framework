4c901ca8e4052a3c6db3999cabfe57af030ff531
Your Name <you@example.com>
2018-06-23 14:09:28 -0400

    Add Symfony Http Foundation

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,8 +4,16 @@ define('ROOT_DIR', dirname(__DIR__));
 
 REQUIRE ROOT_DIR . '/vendor/autoload.php';
 
-use Tracy\Debugger;
+use Tracy\Debugger as TracyDebugger;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
-Debugger::enable();
+TracyDebugger::enable();
 
-echo 'Hello from the bootstrap file :)';
+$request = SymfonyRequest::createFromGlobals();
+
+$content = 'Hello ' . $request->query->get('name', 'visitor');
+
+$response = new SymfonyResponse($content);
+$response->prepare($request);
+$response->send();
