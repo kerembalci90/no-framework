aa360af4b5e0a8f4c201ba5e88866f9a3ee045c5
Your Name <you@example.com>
2018-06-23 16:05:54 -0400

    Add routing

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,19 +1,54 @@
 <?php declare(strict_types=1);
 
 define('ROOT_DIR', dirname(__DIR__));
-
 REQUIRE ROOT_DIR . '/vendor/autoload.php';
 
 use Tracy\Debugger as TracyDebugger;
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
-use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\HttpFoundation\Request as Request;
+use Symfony\Component\HttpFoundation\Response as Response;
+use FastRoute as FastRoute;
+use FastRoute\RouteCollector as RouteCollector;
+use FastRoute\Dispatcher as Dispatcher;
 
 TracyDebugger::enable();
 
-$request = SymfonyRequest::createFromGlobals();
+$request = Request::createFromGlobals();
 
-$content = 'Hello ' . $request->query->get('name', 'visitor');
+$dispatcher = FastRoute\simpleDispatcher(
+    function (RouteCollector $r) {
+        $routes = include(ROOT_DIR . '/src/Routes.php');
+        foreach ($routes as $route)
+        {
+            $r->addRoute(...$route);
+        }
+    }
+);
 
-$response = new SymfonyResponse($content);
+$routeInfo = $dispatcher->dispatch(
+    $request->getMethod(),
+    $request->getPathInfo()
+);
+
+switch ($routeInfo[0]) {
+    case Dispatcher::NOT_FOUND:
+        $response = new Response('Not Found', 404);
+        break;
+    case Dispatcher::METHOD_NOT_ALLOWED:
+        $response = new Response('Not Allowed', 405);
+        break;
+    case Dispatcher::FOUND:
+        [$controllerName, $method] = explode('#', $routeInfo[1]);
+        $vars = $routeInfo[2];
+
+        $controller = new $controllerName;
+        $response = $controller->$method($request, $vars);
+        break;
+}
+
+if(!$response instanceof Response)
+{
+    throw new \Exception("Controller methods must return a Response object");
+}
+
 $response->prepare($request);
 $response->send();
