7aa5f90cf5b0a45ebbd300e458b9fdd1e3f9ed9f
Your Name <you@example.com>
2018-06-25 22:01:48 -0400

    Add a layout template to extend from

--- src/FrontPage/Presentation/FrontPageController.php
+++ src/FrontPage/Presentation/FrontPageController.php
@@ -17,7 +17,12 @@ final class FrontPageController {
 
     public function show(Request $request): Response
     {
-        $content = $this->templateRenderer->render('FrontPage.html.twig');
+        $submissions = [
+            ['url' => "http://google.com", 'title' => 'Google<script>alert(1)</script>'],
+            ['url' => "http://bing.com", 'title' => 'Bing']
+        ];
+
+        $content = $this->templateRenderer->render('FrontPage.html.twig', ['submissions' => $submissions]);
         return new Response($content);
     }
 }
