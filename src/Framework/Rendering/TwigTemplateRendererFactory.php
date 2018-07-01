0a3661b653071a9d622c846c08ea68d656b18746
Kerem Balci <kerembalci90@gmail.com>
2018-07-01 11:09:45 -0400

    Complete CSRF protection by using Flash messages and PRG pattern

--- src/Framework/Rendering/TwigTemplateRendererFactory.php
+++ src/Framework/Rendering/TwigTemplateRendererFactory.php
@@ -3,6 +3,8 @@
 namespace SocialNews\Framework\Rendering;
 
 use SocialNews\Framework\Csrf\StoredTokenReader;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Twig_Loader_Filesystem as TwigLoaderFilesystem;
 use Twig_Environment as TwigEnvironment;
 use Twig_Function as TwigFunction;
@@ -11,11 +13,13 @@ final class TwigTemplateRendererFactory
 {
     private $templateDirectory;
     private $storedTokenReader;
+    private $session;
 
-    public function __construct(TemplateDirectory $templateDirectory, StoredTokenReader $storedTokenReader)
+    public function __construct(TemplateDirectory $templateDirectory, StoredTokenReader $storedTokenReader, Session $session)
     {
         $this->templateDirectory = $templateDirectory;
         $this->storedTokenReader = $storedTokenReader;
+        $this->session = $session;
     }
 
     public function  create() : TwigTemplateRenderer
@@ -23,6 +27,12 @@ final class TwigTemplateRendererFactory
         $templateDirectory = $this->templateDirectory->toString();
         $loader = new TwigLoaderFilesystem([$templateDirectory]);
         $twigEnvironment = new TwigEnvironment($loader);
+
+        $twigEnvironment->addFunction(
+            new TwigFunction('get_flash_bag', function(): FlashBagInterface {
+                return $this->session->getFlashBag();
+            })
+        );
 
         $twigEnvironment->addFunction(
             new TwigFunction('get_token', function (string $key): string {
--- src/Submission/Presentation/SubmissionController.php
+++ src/Submission/Presentation/SubmissionController.php
@@ -3,19 +3,24 @@
 namespace SocialNews\Submission\Presentation;
 
 use SocialNews\Framework\Csrf\StoredTokenValidator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request as Request;
 use Symfony\Component\HttpFoundation\Response as Response;
 use SocialNews\Framework\Rendering\TemplateRenderer as TemplateRenderer;
+use Symfony\Component\HttpFoundation\Session\Session;
+use SocialNews\Framework\Csrf\Token;
 
 final class SubmissionController {
 
     private $templateRenderer;
     private $storedTokenValidator;
+    private $session;
 
-    public function __construct(TemplateRenderer $templateRenderer, StoredTokenValidator $storedTokenValidator)
+    public function __construct(TemplateRenderer $templateRenderer, StoredTokenValidator $storedTokenValidator, Session $session)
     {
         $this->templateRenderer = $templateRenderer;
         $this->storedTokenValidator = $storedTokenValidator;
+        $this->session = $session;
     }
 
     public function show(Request $request): Response
@@ -26,7 +31,15 @@ final class SubmissionController {
 
     public function submit(Request $request): Response
     {
-        $content = $request->get('title') . ' - ' . $request->get('url');
-        return new Response($content);
+        $response = new RedirectResponse('/submit');
+
+        if(!$this->storedTokenValidator->validate('submission', new Token((string)$request->get('token')))) {
+            $this->session->getFlashBag()->add('errors', 'Invalid token');
+            return $response;
+        }
+
+        $this->session->getFlashBag()->add('success', 'Your URL was submitted successfully');
+
+        return $response;
     }
 }
